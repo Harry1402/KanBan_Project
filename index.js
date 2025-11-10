d8c7b43962d671b7923d9cdc889bbf12cd4fea96
hari Rajak <133476890+Harry1402@users.noreply.github.com>
2025-11-10 23:41:47 +0530

    Adding Theme Funnctionality

--- index.js
+++ index.js
@@ -66,7 +66,22 @@ DropWalaBoard.forEach((Board )=> {
 
 
 
+// --------------------------DarkMode Functionality--------------------------
 
+const togglebutton =document.getElementById("theme-toggle");
+const body =document.body ;
+
+body.classList.add('light-mode');
+
+togglebutton.addEventListener("click",()=>{
+    if(body.classList.contains("light-mode")){
+        body.classList.replace("light-mode","dark-mode");
+        togglebutton.textContent ="switch to   light mode";
+    }else{
+        body.classList.replace("dark-mode","light-mode");
+        togglebutton.textContent ="Switch to dark mode";
+    }
+})
 
 
 
